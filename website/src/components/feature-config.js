84449478455eedb93e545d3598e394f81e79b85b
James McLeod <james.mcleod@finos.org>
2021-06-15 15:39:06 +0100

    configure featured projects

--- website/src/components/feature-config.js
+++ website/src/components/feature-config.js
@@ -9,15 +9,43 @@ export const features = [
             How to use the traceability of crypto to limit distribution of child sexual abuse material (CSAM).
             </>
         ),
+        about: {
+            title: <>Read more about the project</>,
+            link: ''
+        },        
+        project: {
+            title: <>Visit the project on FINOS Labs</>,
+            link: 'https://github.com/finos-labs/csam-crypto'
+        },        
+        involved: {
+            title: <>Get Involved</>,
+            link: ''
+        }  
     },
     {
         title: <>Women’s Economic Empowerment</>,
         imageUrl: '/img/intro/AIR_Icon.png',
+        about: {
+            title: '',
+            link: ''
+        },
         description: (
             <>
             Solutions to help vulnerable women attain financial independence and security.
             </>
         ),
+        about: {
+            title: <>Read more about the project</>,
+            link: ''
+        },                
+        project: {
+            title: <>Visit the project on FINOS Labs</>,
+            link: 'https://github.com/finos-labs/weets-bill-pay-it-forward'
+        },        
+        involved: {
+            title: <>Get Involved</>,
+            link: ''
+        }  
     },
     {
         title: <>NYDFS (New York Department of Financial Services) Digital Regulatory Reporting for Cryptocurrency</>,
@@ -26,6 +54,18 @@ export const features = [
             <>
             Digital Regulatory Reporting (DRR) will to give regulators instant access to more complete data provided by firms under their supervision.
             </>
-        )
+        ),
+        about: {
+            title: <>Read more about the project</>,
+            link: ''
+        },                
+        project: {
+            title: <>Visit the project on FINOS Labs</>,
+            link: 'https://github.com/finos-labs/nydfs-digital-regulatory-reporting'
+        },        
+        involved: {
+            title: <>Get Involved</>,
+            link: ''
+        }  
     }
 ];
--- website/src/components/feature.js
+++ website/src/components/feature.js
@@ -3,7 +3,7 @@ import classnames from 'classnames';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from '../pages/styles.module.css';
 
-export default function Feature({imageUrl, title, description}) {
+export default function Feature({imageUrl, title, description, about, project, involved}) {
     const imgUrl = useBaseUrl(imageUrl);
     return (
         <div className={classnames('text--center col col--4', styles.feature)}>
@@ -14,6 +14,9 @@ export default function Feature({imageUrl, title, description}) {
         )}
         <h3>{title}</h3>
         <p>{description}</p>
+        <p className={classnames(styles.cta)}><a href={about.link}>{about.title}</a></p>
+        <p className={classnames(styles.cta)}><a href={project.link}>{project.title}</a></p>
+        <p className={classnames(styles.cta)}><a href={involved.link}>{involved.title}</a></p>
         </div>
     );
 }
