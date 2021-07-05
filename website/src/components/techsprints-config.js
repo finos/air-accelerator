67c75ac8ecc10d2bb4fa216caca1aab00a24edb1
James McLeod <james.mcleod@finos.org>
2021-07-05 15:21:04 +0100

    rename vendor to techsprints

---
+++ website/src/components/techsprints-config.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const vendors = [
+    {
+        title: <></>,
+        imageUrl: '/img/intro/AIR_Icon.png',
+        description: (
+            <></>
+        ),
+    },
+    {
+        title: <></>,
+        imageUrl: '/img/intro/AIR_Icon.png',
+        description: (
+            <></>
+        ),
+    },
+    {
+        title: <></>,
+        imageUrl: '/img/intro/AIR_Icon.png',
+        description: (
+            <></>
+        ),
+    }
+];
