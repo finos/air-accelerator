2d5af0f98968eb215aa1f6d619f68cbe70c9820f
James McLeod <james.mcleod@finos.org>
2021-07-05 15:07:48 +0100

    add bullets to middle panels

--- website/src/components/feature.js
+++ website/src/components/feature.js
@@ -7,16 +7,24 @@ export default function Feature({imageUrl, title, description, about, project, i
     const imgUrl = useBaseUrl(imageUrl);
     return (
         <div className={classnames('text--center col col--4 padding', styles.feature)}>
-        {imgUrl && (
-            <div>
-            <img className={styles.featureImage} src={imgUrl} alt={title} />
+            {imgUrl && (
+                <div>
+                <img className={styles.featureImage} src={imgUrl} alt={title} />
+                </div>
+            )}
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <div className={classnames(styles.featureCTA)}>
+                <ul>
+                    <li><a href={about.link}>{about.title}</a></li>
+                </ul>
+                <ul>
+                    <li><a href={project.link}>{project.title}</a></li>
+                </ul>
+                <ul>
+                    <li><a href={involved.link}>{involved.title}</a></li>
+                </ul>
             </div>
-        )}
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <p className={classnames(styles.cta)}><a href={about.link}>{about.title}</a></p>
-        <p className={classnames(styles.cta)}><a href={project.link}>{project.title}</a></p>
-        <p className={classnames(styles.cta)}><a href={involved.link}>{involved.title}</a></p>
         </div>
     );
 }
