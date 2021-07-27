b7a8bf1a79bf42bbf4c67bddb1dfcc8a4ab472db
James McLeod <james.mcleod@finos.org>
2021-07-27 14:54:33 +0100

    update womens economic empowerment info

--- website/src/components/feature-config.js
+++ website/src/components/feature-config.js
@@ -2,28 +2,26 @@ import React from 'react';
 
 export const features = [
     {
-        title: <>Cryptocurrency &amp; Child Protection</>,
+        title: <>Cryptocurrency and Child Protection</>,
         imageUrl: '/img/intro/AIR_Icon.png',
         description: (
-            <>
-            AIR hosted a TechSprint with regulators, law enforcement, industry and experts to explore how to use the traceability of crypto to limit distribution of child sexual abuse material (CSAM). The prototype tools are being further developed for use by FinCEN and law enforcement.
-            </>
+            <>Development of prototype tools for use by FinCEN and law enforcement agencies to identify cryptocurrency wallets engaged in the distribution of child sexual abuse material (CSAM).</>
         ),
         about: {
             title: <>Learn more about the project</>,
             link: 'docs/crypto-and-child-protection'
         },        
         project: {
-            title: <>Visit the project on FINOS Labs</>,
-            link: 'https://github.com/finos-labs/csam-crypto'
+            title: '',
+            link: ''
         },        
         involved: {
-            title: <>Get Involved</>,
-            link: 'docs/crypto-and-child-protection#interested-in-contributing'
+            title: '',
+            link: ''
         }  
     },
     {
-        title: <>Women’s Economic Empowerment / Bill Pay It Forward</>,
+        title: <>Women’s Economic Empowerment</>,
         imageUrl: '/img/intro/AIR_Icon.png',
         about: {
             title: '',
@@ -31,7 +29,8 @@ export const features = [
         },
         description: (
             <>
-            In March 2021, AIR and the <a href='https://www.fca.org.uk/'>FCA</a> partnered to raise awareness for global Women’s Economic Empowerment and spur development of tech solutions to help vulnerable women achieve financial security and independence. Bill Pay It Forward is a winning solution focused on using self-declared data to "calculate" an alternative credit score.
+            <p>Creation of a new Financial Stability Score to help identify and support vulnerable women emerging from the COVID financial crisis achieve financial security and independence.</p>
+            <p>In March 2021, AIR and the <a href='https://www.fca.org.uk/'>FCA</a> partnered to raise awareness for global Women’s Economic Empowerment and spur development of tech solutions to help vulnerable women achieve financial security and independence. Bill Pay It Forward is a winning solution focused on using self-declared data to “calculate” an alternative credit score.</p>
             </>
         ),
         about: {
